c6ebfab60cd76c7b9783476dcce3e2ad76a29b27
kamilah santos <kahsantos89@gmail.com>
2020-08-13 23:05:06 -0300

    eureka server

---
+++ src/test/java/com/livecoding/dio/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livecoding.dio.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
